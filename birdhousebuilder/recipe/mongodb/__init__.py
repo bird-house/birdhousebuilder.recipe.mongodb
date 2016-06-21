a81fb0a37a9984796dfcc083b2463dc73eeeb2ac
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-06-21 20:00:24 +0200

    using zc.recipe.deployment

--- birdhousebuilder/recipe/mongodb/__init__.py
+++ birdhousebuilder/recipe/mongodb/__init__.py
@@ -4,13 +4,15 @@
 
 import os
 from mako.template import Template
+import logging
 
 import zc.buildout
+import zc.recipe.deployment
 from birdhousebuilder.recipe import conda, supervisor
 from birdhousebuilder.recipe.conda import conda_env_path
 
 templ_config = Template(filename=os.path.join(os.path.dirname(__file__), "mongodb.conf"))
-templ_cmd = Template('${env_path}/bin/mongod --config ${etc_prefix}/mongodb.conf') 
+templ_cmd = Template('${env_path}/bin/mongod --config ${etc_directory}/mongodb.conf') 
 
 class Recipe(object):
     """This recipe is used by zc.buildout.
@@ -20,15 +22,21 @@ class Recipe(object):
         self.buildout, self.name, self.options = buildout, name, options
         b_options = buildout['buildout']
 
-        deployment = self.deployment = options.get('deployment')
-        if deployment:
-            self.options['prefix'] = buildout[deployment].get('prefix')
-            self.options['etc_prefix'] = buildout[deployment].get('etc-prefix')
-            self.options['var_prefix'] = buildout[deployment].get('var-prefix')
-        else:
-            self.options['prefix'] = os.path.join(buildout['buildout']['parts-directory'], self.name)
-            self.options['etc_prefix'] = os.path.join(self.options['prefix'], 'etc')
-            self.options['var_prefix'] = os.path.join(self.options['prefix'], 'var')
+        self.options['name'] = self.options.get('name', self.name)
+        self.name = self.options['name']
+
+        self.logger = logging.getLogger(name)
+
+        deployment = zc.recipe.deployment.Install(buildout, 'mongodb', {
+                                                'prefix': self.options['prefix'],
+                                                'user': self.options['user'],
+                                                'etc-user': self.options['user']})
+        deployment.install()
+        
+        self.options['etc-prefix'] = self.options['etc_prefix'] = deployment.options['etc-prefix']
+        self.options['var-prefix'] = self.options['var_prefix']  = deployment.options['var-prefix']
+        self.options['etc-directory'] = self.options['etc_directory'] = deployment.options['etc-directory']
+        self.options['lib-directory'] = self.options['lib_directory'] = deployment.options['lib-directory']
         self.prefix = self.options['prefix']
 
         self.env_path = conda_env_path(buildout, options)
@@ -54,35 +62,25 @@ class Recipe(object):
             self.name,
             {'pkgs': 'mongodb'})
 
-        conda.makedirs( os.path.join(self.options['var_prefix'], 'lib', 'mongodb') )
-        #conda.makedirs( os.path.join(self.prefix, 'var', 'log', 'mongodb') )
-        
         return script.install()
         
     def install_config(self):
         """
         install mongodb config file
         """
-        result = templ_config.render(**self.options)
-    
-        output = os.path.join(self.options['etc_prefix'], 'mongodb.conf')
-        conda.makedirs(os.path.dirname(output))
+        text = templ_config.render(**self.options)
+        conf_path = os.path.join(self.options['etc-directory'], 'mongodb.conf')
         
-        try:
-            os.remove(output)
-        except OSError:
-            pass
-
-        with open(output, 'wt') as fp:
-            fp.write(result)
-        return [output]
+        with open(conf_path, 'wt') as fp:
+            fp.write(text)
+        return [conf_path]
 
     def install_program(self):
         script = supervisor.Recipe(
             self.buildout,
             self.name,
-            {'user': self.options.get('user'),
-             'deployment': self.deployment,
+            {'prefix': self.options['prefix'],
+             'user': self.options.get('user'),
              'program': 'mongodb',
              'command': templ_cmd.render(**self.options),
              'priority': '10',
